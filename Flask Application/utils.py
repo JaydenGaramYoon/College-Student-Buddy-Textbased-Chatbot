cba63d18d336fa8267961b4b5d9a9dbf722f10ff
GaramYoon/Jayden <159978850+JaydenGaramYoon@users.noreply.github.com>
2025-01-03 00:38:08 -0500

    Update utils.py

--- Flask Application/utils.py
+++ Flask Application/utils.py
@@ -15,35 +15,21 @@ from keras.src.saving import saving_api
 from keras.src.backend.tensorflow.trainer import TensorFlowTrainer 
 from keras.src.models.model import Model
 
-# def clean_up_sentence(sentence):
-#     lemmatizer = WordNetLemmatizer()
-
-#     sentence_words = nltk.word_tokenize(sentence)
-#     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-
-#     return sentence_words
-
-
-# def bag_of_words(sentence):
-    # words = pickle.load(open('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/words.pkl', 'rb'))
-
-    # sentence_words = clean_up_sentence(sentence)
-    # bag = [0] * len(words)
-    # for w in sentence_words:
-    #     for i, word in enumerate(words):
-    #         if word == w:
-    #             bag[i] = 1
-    # return np.array(bag)
-
 classes = pickle.load(open('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/classes.pkl', 'rb'))
 model = saving_api.load_model('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/chatbot_model.keras')
 
 
 def bag_of_words(sentence):
-    lemmatizer = WordNetLemmatizer()
+     lemmatizer = WordNetLemmatizer()
+    
+    phrases_to_tokenize = ['goodbye', 'good bye', 'good night', 'goodnight', 'good day', 'good one']
+    for phrase in phrases_to_tokenize:
+        sentence = sentence.replace(phrase, 'cya')
+    
     sentence_words = nltk.word_tokenize(sentence)
+   
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-
+    
     # Load words from your training data
     words = pickle.load(open('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/words.pkl', 'rb'))
     bag = [0] * len(words)
@@ -60,7 +46,6 @@ def predict_class(sentence):
 
     bow = bag_of_words(sentence)
     res = Model.predict(model, np.array([bow]))[0]
-    # res = trainer.predict(model, np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
 
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
