66c325622a315a40474c13876fb2a3e66d987e4e
GaramYoon/Jayden <159978850+JaydenGaramYoon@users.noreply.github.com>
2025-01-07 00:23:38 -0500

    Update utils.py

--- Flask Application/utils.py
+++ Flask Application/utils.py
@@ -1,8 +1,3 @@
-from importlib import import_module
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import random
 import json
 import pickle
@@ -12,40 +7,48 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 from keras.src.saving import saving_api
-from keras.src.backend.tensorflow.trainer import TensorFlowTrainer 
 from keras.src.models.model import Model
 
 classes = pickle.load(open('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/classes.pkl', 'rb'))
 model = saving_api.load_model('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/chatbot_model.keras')
 
+def clean_up_sentence(sentence):
+    lemmatizer = WordNetLemmatizer()
+
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+
+    return sentence_words
 
 def bag_of_words(sentence):
-     lemmatizer = WordNetLemmatizer()
+    lemmatizer = WordNetLemmatizer()
     
+    # List of phrases to replace with 'cya'
     phrases_to_tokenize = ['goodbye', 'good bye', 'good night', 'goodnight', 'good day', 'good one']
     for phrase in phrases_to_tokenize:
-        sentence = sentence.replace(phrase, 'cya')
+        sentence = sentence.replace(phrase, 'cya')  # Replace phrases with 'cya'
     
-    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = nltk.word_tokenize(sentence)  
    
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     
-    # Load words from your training data
+ 
     words = pickle.load(open('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/words.pkl', 'rb'))
-    bag = [0] * len(words)
+    bag = [0] * len(words) 
     for s in sentence_words:
         for i, w in enumerate(words):
             if w == s:
-                bag[i] = 1
+                bag[i] = 1  
 
-    return np.array(bag)
+    return np.array(bag)  
 
 def predict_class(sentence):
     classes = pickle.load(open('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/classes.pkl', 'rb'))
     model = saving_api.load_model('c:/Users/윤가람/OneDrive/바탕 화면/PROJECTS/AI_Chatbot/Current/Flask Application/model/chatbot_model.keras')
 
     bow = bag_of_words(sentence)
-    res = Model.predict(model, np.array([bow]))[0]
+    bow = np.pad(bow, (0, max(0, 56 - len(bow))), 'constant')[:56]
+    res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
 
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
@@ -57,7 +60,7 @@ def predict_class(sentence):
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
 
 
-    print(f"Predicted classes: {return_list}")  # 디버깅을 위한 로그 추가
+    print(f"Predicted classes: {return_list}")  
     
     return return_list
 
